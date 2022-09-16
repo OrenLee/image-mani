75d64ee2a2efc5398482162a35373e3fdb8fadb8
Oren Lee <orenlee8@gmail.com>
2022-09-16 23:30:05 +0300

    prod static folder

--- src/main/java/com/orenlee/imagemani/security/WebMvcConfig.java
+++ src/main/java/com/orenlee/imagemani/security/WebMvcConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.io.IOException;
 
-@Configuration
+
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
