191499a9e280f6ec46df974323d9b46e226a6786
Oren Lee <orenlee8@gmail.com>
2022-09-17 13:27:51 +0300

    prod

--- src/main/java/com/orenlee/imagemani/controllers/ViewController.java
+++ src/main/java/com/orenlee/imagemani/controllers/ViewController.java
@@ -3,7 +3,7 @@ package com.orenlee.imagemani.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+
 public class ViewController {
 
     @RequestMapping({ "/login", "/signup", "/images", "/verify-account", "/change-password" })
