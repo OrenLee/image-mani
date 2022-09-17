d47842b575c88958ea44cef3d14987c858a41d62
Oren Lee <orenlee8@gmail.com>
2022-09-17 12:52:21 +0300

    prod static folder

---
+++ src/main/java/com/orenlee/imagemani/controllers/ViewController.java
@@ -0,0 +1,13 @@
+package com.orenlee.imagemani.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+    @RequestMapping({ "/login" })
+    public String index() {
+        return "forward:/index.html";
+    }
+}
