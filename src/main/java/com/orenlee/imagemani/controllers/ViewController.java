6e16e5b2cce4aa6f0e41a4dfdc87e1496e94c6fb
Oren Lee <orenlee8@gmail.com>
2022-09-17 14:38:30 +0300

    prod

--- src/main/java/com/orenlee/imagemani/controllers/ViewController.java
+++ src/main/java/com/orenlee/imagemani/controllers/ViewController.java
@@ -2,8 +2,6 @@ package com.orenlee.imagemani.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 
 @Controller
 public class ViewController {
